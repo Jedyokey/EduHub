e86c5a439b64720f570d86ac7c6ff6e1fb2c10f1
Jedyokey <jedyokey29@gmail.com>
2024-12-22 08:16:46 +0000

    Updated campus section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import Programs from './components/Programs/Programs'
+import Title from './components/Title/Title'
+import About from './components/About/About'
+import Campus from './components/Campus/Campus'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Title subTitle="OUR PROGRAM" title="What We Offer" />
+        <Programs />
+        <About />
+        <Title subTitle="Gallery" title="Campus Photos" />
+        <Campus />
+      </div>
+      
       
     </div>
   )
